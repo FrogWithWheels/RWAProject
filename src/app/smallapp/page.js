62d98b9060fc628ff7a02e1bb6277c05829603b5
NotABusDriver <jakecallow42@gmail.com>
2024-11-18 12:28:46 +0000

    Initial commit.

---
+++ src/app/smallapp/page.js
@@ -0,0 +1,71 @@
+'use client'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import {useState} from 'react';
+
+export default function MyApp() {
+	const [showLogin, setShowLogin] = useState(false);
+	const [showDash, setShowDash] = useState(false);
+	const [showFirstPage, setShowFirstPage] = useState(true);
+
+	function runShowLogin() {
+		setShowLogin(true);
+		setShowDash(false);
+		setShowFirstPage(false);
+	}
+
+	function runShowDash() {
+		setShowLogin(false);
+		setShowDash(true);
+		setShowFirstPage(false);
+	}
+
+	function runShowFirstPage() {
+		setShowLogin(false);
+		setShowDash(false);
+		setShowFirstPage(true);
+	}
+
+	return (
+		<Box sx={{ flexGrow: 1 }}>
+			<AppBar position="static">
+				<Toolbar>
+					<IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+						<MenuIcon/>
+					</IconButton>
+					<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+						MyApp
+					</Typography>
+					<Button color="inherit" onClick={runShowFirstPage}>First</Button>
+					<Button color="inherit" onClick={runShowLogin}>Login</Button>
+					<Button color="inherit" onClick={runShowDash}>Dash</Button>
+				</Toolbar>
+			</AppBar>
+			{showFirstPage && 
+				<Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+				This is a very basic application. This has a bar cross the top and this box!
+				How this app works is by creating two boxes. They are hidden in the background of this page.
+				It is only when a user clicks one of the buttons that we change the "state" from hidden (false) to show (true)
+				</Box>
+			}
+
+			{showLogin && 
+				<Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+				This box is hidden until you click the button! Imagine this is one page in your app!
+				</Box>
+			}
+
+			{showDash && 
+				<Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+				Let's pretend this is the dashboard!
+				</Box>
+			}
+		</Box>
+	);
+}
