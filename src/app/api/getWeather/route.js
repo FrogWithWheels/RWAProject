a2606d75c7f7acb621afc4f0b937bc00c8a66a0c
NotABusDriver <jakecallow42@gmail.com>
2024-12-06 14:10:41 +0000

    Weather API fix.

--- src/app/api/getWeather/route.js
+++ src/app/api/getWeather/route.js
@@ -1,6 +1,7 @@
 export async function GET(req, res) {
 	console.log("Weather API accessed.");
 
+	revalidatePath("http://api.weatherapi.com/v1/current.json?key=7aa17f02465349f7ac4160217242611&q=Dublin&aqi=no");
 	const weather = await fetch('http://api.weatherapi.com/v1/current.json?key=7aa17f02465349f7ac4160217242611&q=Dublin&aqi=no');
 	const data = await weather.json();
 
