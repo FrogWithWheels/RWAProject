4b35777af1b66ed6e04d076e03177a6951f49802
NotABusDriver <jakecallow42@gmail.com>
2024-12-15 13:48:07 +0000

    Problem with session function.

--- src/app/api/sessionCode.js
+++ src/app/api/sessionCode.js
@@ -1,7 +1,7 @@
 import { getIronSession } from 'iron-session';
 import { cookies } from 'next/headers'
 
-export async function getCustomSession(){
+export default async function getCustomSession(){
     console.log("Sessions loaded");
 
     let pw = "VIi8pH38vD8ZLgEZclSa7an3olx4pkh6pvBj9fGZf";
